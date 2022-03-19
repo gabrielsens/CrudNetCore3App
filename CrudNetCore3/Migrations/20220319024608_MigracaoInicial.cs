c8a9b4b6e59bce1da344561e7bc18f117343cb40
Gabriel Sens <gabriel_sens@hotmail.com>
2022-03-19 00:22:57 -0300

    Criação de um crud simples usando net core 3.1 e scaffolded para criação das rotas, views e controller

---
+++ CrudNetCore3/Migrations/20220319024608_MigracaoInicial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CrudNetCore3.Migrations
+{
+    public partial class MigracaoInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Cpf = table.Column<string>(nullable: true),
+                    Telefone = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
---
+++ CrudNetCore3/Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿namespace CrudNetCore3.Models
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+        public string Telefone { get; set; }
+    }
+}
